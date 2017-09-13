705871e7c9f75487718e5923dbc5d9c0b7b615db
Mykola Samila <samila.mykola@gmail.com>
2017-09-13 14:45:29 +0300

    Apartment list

---
+++ src/app/apartments/apartments-list/apartments-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Apartment } from '../../shared/apartment.model';
+
+@Component({
+  selector: 'app-apartments-list',
+  templateUrl: './apartments-list.component.html',
+  styleUrls: ['./apartments-list.component.css']
+})
+export class ApartmentsListComponent implements OnInit {
+
+	apartments: Apartment[] = [
+		new Apartment('Two level flat', 1),
+		new Apartment('4 room flat', 2)
+	];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/apartment.model.ts
@@ -0,0 +1,3 @@
+export class Apartment {
+	constructor(public name: string, public amount: number) {}
+}
